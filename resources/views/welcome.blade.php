61a44e642aaec4fe97ba7390ac2d24c2a59f4199
Nicowski25 <nicola.faedo0610@gmail.com>
2023-05-31 12:54:36 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title','Home')
+@section('content')
+<div class="p-5 mb-4 bg-light rounded-3">
+    <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Home</h1>
+    </div>
+</div>
+@endsection
